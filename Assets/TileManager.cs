47a7390e9276b7e568d3153343be2dfcb78f86ec
Shirini02 <shirini.gone@xcubelabs.com>
2021-05-27 00:38:49 -0700

    Tiles Generation in zig zag format

---
+++ Assets/TileManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileManager : MonoBehaviour
+{
+    public GameObject[] tiles;
+    public GameObject currentTile;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            SpawnTile();
+        }
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void SpawnTile()
+    {
+        int index = Random.Range(0, tiles.Length);
+        currentTile = (GameObject)Instantiate(tiles[index], currentTile.transform.GetChild(index).position, Quaternion.identity);
+    }
+
+}
